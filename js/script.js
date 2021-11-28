3ab66b23942ebc4b972bdb1482d581c822e75edb
israel <israel.pereira@dcx.ufpb.br>
2021-11-28 16:09:16 -0300

    modificaçoes nos files

--- js/script.js
+++ js/script.js
@@ -1,45 +1,72 @@
-// variaveis
-var imgQtd = 0;
-var isParado = false;
-var slideAtual = 0;
+// // variaveis
+// var imgQtd = 0;
+// var isParado = false;
+// var slideAtual = 0;
 
-ajax('fotos.json', 'GET', (resposta) => {
-  let fotos = JSON.parse(resposta)
-  imgQtd = fotos.length
+// ajax('fotos.json', 'GET', (resposta) => {
+//   let fotos = JSON.parse(resposta)
+//   imgQtd = fotos.length
 
-  for (let item of fotos) {
-    console.log(item)
-    document.querySelector('.slider .painel').innerHTML +=
-      `<div class="item" style="background-image: url(${item['url']});">
-        <div class="info">
-          <h3 class="titulo">${item['info']['titulo']}</h3>
-          <p class="subtitulo">${item['info']['subtitulo']}</p>
-        </div>
-      </div>`
-  }
-  // pegando e calculando a quantidade de slideshow
-  document.querySelector('.slider .painel').style.width = `calc(100vw * ${imgQtd})`
+//   for (let item of fotos) {
+//     console.log(item)
+//     document.querySelector('.slider .painel').innerHTML +=
+//       `<div class="item" style="background-image: url(${item['url']});">
+//         <div class="info">
+//           <h3 class="titulo">${item['info']['titulo']}</h3>
+//           <p class="subtitulo">${item['info']['subtitulo']}</p>
+//         </div>
+//       </div>`
+//   }
+//   // pegando e calculando a quantidade de slideshow
+//   document.querySelector('.slider .painel').style.width = `calc(100vw * ${imgQtd})`
 
-  setInterval(() => { if (! isParado) avancar() }, 8000)
-})
+//   setInterval(() => { if (! isParado) avancar() }, 8000)
+// })
+
+// function voltar() {
+//   slideAtual = (slideAtual > 0 ? slideAtual : imgQtd) -1
+//   atualizarMargem()
+// }
+
+// function togglePausar() {
+//   let iconeClasse = `fas fa-${isParado ? 'pause' : 'play'}`
+//   document.querySelector('.slider .controle.pausa span').className = iconeClasse
+//   isParado = ! isParado
+// }
+
+// function avancar() {
+//   slideAtual = slideAtual >= (imgQtd -1) ? 0 : slideAtual +1
+//   atualizarMargem()
+// }
+
+// function atualizarMargem() {
+//   let novaMargem = slideAtual * document.querySelector('.slider .item').clientWidth
+//   document.querySelector('.slider .painel').style.marginLeft = `-${novaMargem}px`
+// }
+
+let totalSlides = document.querySelectorAll('.item').length;
+let currentSlide = 0;
+
+document.querySelector('.painel').style.width = `calc(100vw * ${totalSlides})`;
+document.querySelector('.controles').style.height = `${document.querySelector('.slider').clientHeight}px`;
 
 function voltar() {
-  slideAtual = (slideAtual > 0 ? slideAtual : imgQtd) -1
-  atualizarMargem()
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = totalSlides - 1;
+  }
+  updateMargin();
 }
-
-function togglePausar() {
-  let iconeClasse = `fas fa-${isParado ? 'pause' : 'play'}`
-  document.querySelector('.slider .controle.pausa span').className = iconeClasse
-  isParado = ! isParado
+function avançar() {
+  currentSlide++;
+  if (currentSlide > (totalSlides - 1)){
+    currentSlide = 0;
+  }
+  updateMargin();
 }
-
-function avancar() {
-  slideAtual = slideAtual >= (imgQtd -1) ? 0 : slideAtual +1
-  atualizarMargem()
+function updateMargin() {
+  let slideItemWidth = document.querySelector('.item').clientWidth;
+  let newMargin = (currentSlide * slideItemWidth);
+  document.querySelector('.painel').style.marginLeft = `-${newMargin}px`;
 }
-
-function atualizarMargem() {
-  let novaMargem = slideAtual * document.querySelector('.slider .item').clientWidth
-  document.querySelector('.slider .painel').style.marginLeft = `-${novaMargem}px`
-}
+setInterval(avançar, 5000);
